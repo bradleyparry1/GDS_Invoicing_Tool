f11c7d6198382b5edba208086620a7febe832183
bradleydavidparry <bradley.parry@digital.cabinet-office.gov.uk>
2020-02-28 21:54:36 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      inlineSource: '.(js|css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+    new CopyPlugin([
+        'code.js',
+        'appsscript.json',
+        '.clasp.json'
+    ])
+  ]
+};
